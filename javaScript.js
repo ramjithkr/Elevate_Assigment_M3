b1e474f37b48b45fbb524b4688f952842dfd942d
ramjith kr <ramjithkr5441@gmail.com>
2024-04-05 21:24:16 +0530

    commit

---
+++ javaScript.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const divs = document.querySelectorAll('.div');
+    divs.forEach(div => {
+        div.addEventListener('mouseover', function() {
+            this.style.transform = 'scale(1.1)';
+            this.style.transition = 'transform 0.5s ease';
+        });
+        div.addEventListener('mouseout', function() {
+            this.style.transform = 'scale(1)';
+            this.style.transition = 'transform 0.5s ease';
+        });
+    });
+});
+
+
+
+
